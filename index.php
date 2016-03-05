de69b077a0c58ee3e917bacbd0cc86d0d831b1a3
Quentin Bouteiller <quentin@quent1.fr>
2016-03-06 00:44:55 +0100

    Using Templacat
    Using Templacat for rendering the template into the application.

--- index.php
+++ index.php
@@ -5,12 +5,35 @@
     // Then, we create a new instance of Slim
     $slim = new \Slim\App();
 
-    // Test
-    $slim->get('/', function(Slim\Http\Request $request, Slim\Http\Response $response, array $args) {
-        $response->write('<h1>Bonjour le monde</h1><p>Nothing to see here...</p>');
-        return $response;
-    });
+    // And a new instance of Templacat (handcrafted templating system)
+    $templacat = new \Templacat\Templacat('template/tpl');
 
-    // And finally we run the app
-    $slim->run();
+    // We load the template files into the templating system
+    $templacat->load_template('header');
+    $templacat->load_template('sidebar', 'PAGE_SIDEBAR'); // The sidebar will be loaded into the PAGE_SIDEBAR variable
+    $templacat->load_template('footer');
+
+    // [TEMP] Our first page
+    $slim->get('/', function(Slim\Http\Request $request, Slim\Http\Response $response, array $args)
+    {
+        global $templacat;
+        $response->write('<h1>Bonjour le monde</h1><p>Nothing to see here...</p>');   
+        $templacat->set_variable('PAGE_TITLE', 'Homepage');
+
+        return $response;
+    })->setName('home');
+
+    // Start buffering the output
+    ob_start();
+        // Render the view from Slim
+        $slim->run();
+
+        // And save the output to a Templacat variable
+        $templacat->set_variable('PAGE_CONTENT', ob_get_contents());
+
+    // Delete the buffer, without outputing the content
+    ob_end_clean();
+    
+    // And finally render the template
+    echo $templacat->render();
 ?>
