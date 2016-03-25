2cf5cb19b9a9249958f662b7e70544c9341d7efa
Quentin Bouteiller <quentin@quent1.fr>
2016-03-25 10:22:57 +0100

    Adding a employee
    You can now add a employee to the database.

--- router.php
+++ router.php
@@ -28,6 +28,9 @@
 	$slim->get('/clients_visualiser', function($request, $response, $args) use ($templacat, $slim, $config){ include 'controllers/clients_visualiser.php'; })->setName('home');
 	
 	$slim->get('/salaries_ajouter', function($request, $response, $args) use ($templacat, $slim){ include 'controllers/salaries_ajouter.php'; })->setName('home');
+	$slim->post('/salaries_ajouter_submit', function($request, $response, $args) use ($templacat, $slim){ include 'controllers/salaries_ajouter_submit.php'; })->setName('home');
+
+	$slim->get('/salaries_editer', function($request, $response, $args) use ($templacat, $slim){ include 'controllers/salaries_editer.php'; })->setName('home');
 	
 	$slim->get('/salaries_voir', function($request, $response, $args) use ($templacat, $slim, $config){ include 'controllers/salaries_voir.php'; })->setName('home');
 	
